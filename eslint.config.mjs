b9ec9e99ea934d871c5c60a348cc4fe3c47a22e8
masoudkaarimi <masoud.develop@gmail.com>
2025-11-07 11:38:28 +0330

    chore: configure eslint, prettier, lint-staged, husky and commit conventions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,6 +22,16 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  prettierRecommended,
 ];
 
 export default eslintConfig;
